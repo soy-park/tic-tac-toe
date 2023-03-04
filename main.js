1e8aee8af33e957ec7c8448d29cb04979359d02e
Sara Park <sspark106@gmail.com>
2023-03-03 18:03:33 -0700

    feat: Update wins on DOM when player wins

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var currentGame = new Game();
 var playerTurnHeading = document.querySelector(".player-turn");
 var boxContainer = document.querySelector('.box-container');
 var box = document.querySelectorAll('.box');
+var player1Wins = document.querySelector('.bee-wins-count');
+var player2Wins = document.querySelector('.flower-wins-count');
 
 window.addEventListener("load", startNewGame);
 boxContainer.addEventListener("click", takeTurn);
@@ -20,6 +22,7 @@ function takeTurn(event) {
     currentGame.playGame(event.target.id);
     displayToken(event);
     currentGame.determineWin();
+    increaseWinCount();
     displayWinner();
     if (currentGame.detectDraw() === "draw") {
         displayDraw();
@@ -31,7 +34,7 @@ function changePlayerTurnHeading() {
         playerTurnHeading.innerHTML = `<span>IT'S</span><img src="${currentGame.player1.token}" alt="bee-emoji"><span>'S TURN!</span>`;
     } else if (currentGame.turn === currentGame.player2.token) {
         playerTurnHeading.innerHTML = `<span>IT'S </span><img src="${currentGame.player2.token}" alt="flower-emoji"><span>'S TURN!</span>`;
-    }
+    } 
 }
 
 function displayToken(event) {
@@ -47,9 +50,11 @@ function displayToken(event) {
 }
 
 // function preventClick(event) {
-//     for (var i = 0; i < box.length; i++) {
-//         if (event.target.id === box[i].id) {
-//             box[i].style.pointerEvents = "none";
+//     if (event.target.id && event.target.innerHTML) {
+//         for (var i = 0; i < box.length; i++) {
+//             if (event.target.id === box[i].id) {
+//                 box[i].style.pointerEvents = "none";
+//             }
 //         }
 //     }
 // }
@@ -58,11 +63,11 @@ function displayWinner() {
     if (currentGame.determineWin() === "player1") {
         playerTurnHeading.innerHTML = `<img src="${currentGame.player1.token}" alt="bee-emoji"><span>WINS!</span>`;
         boxContainer.style.pointerEvents = "none";
-        setTimeout(startNewGame, 2000)
+        setTimeout(startNewGame, 2000);
     } else if (currentGame.determineWin() === "player2") {
         playerTurnHeading.innerHTML = `<img src="${currentGame.player2.token}" alt="flower-emoji"><span>WINS!</span>`; 
-        boxContainer.style.pointerEvents = "none";
-        setTimeout(startNewGame, 2000)
+        boxContainer.style.pointerEvents = "none";  
+        setTimeout(startNewGame, 2000);
     } else {
         currentGame.determineNextTurn();
         changePlayerTurnHeading();
@@ -73,6 +78,14 @@ function displayDraw() {
     playerTurnHeading.innerHTML = `<span>IT'S A DRAW!</span>`; 
     // boxContainer.style.pointerEvents = "none";
     setTimeout(startNewGame, 2000);
+}
+
+function increaseWinCount() {
+    if (currentGame.determineWin() === "player1") {
+        player1Wins.innerText = `WINS: ${currentGame.player1.wins}`;
+    } else if (currentGame.determineWin() === "player2") {
+        player2Wins.innerText = `WINS: ${currentGame.player2.wins}`;
+    }
 }
 
 function clearBoard() {
